cf8b9a27e09214ff325dbf92fbb82b5a52e1f3dd
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-05 16:38:59 -0500

    os module scanning directories

--- disk_report.py
+++ disk_report.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import os
 
 print('Running Script....')
 
@@ -10,3 +11,11 @@ if __name__ == '__main__':
 
     # assigns the directory
     directory = sys.argv[1] if len(sys.argv) >= 2 else path
+
+    # scan all directories and files under (directory)
+    for entry in os.scandir(directory):
+        # print all directories
+        print(entry.path)  # gets full path entry.path
+        # check if entry is a directory and ignoring symbolic links
+        if (entry.is_dir(follow_symlinks=False)):
+            print(entry.path + ' is a directory')
