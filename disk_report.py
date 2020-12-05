731678e0c5886df297ccaba22c9ef539c5c7ead8
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-05 12:28:36 -0500

    getting command line args

--- disk_report.py
+++ disk_report.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+import sys
+
+print('Running Script....')
+
+if __name__ == '__main__':
+    path = '/home'
+    print('total arguments passed: ', len(sys.argv))
+
+    # assigns the directory
+    directory = sys.argv[1] if len(sys.argv) >= 2 else path
